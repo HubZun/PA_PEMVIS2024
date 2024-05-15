6ff8f1a6179376b30dbf56cbca8e9aaa2458e444
ahmadhidayat22 <ahmadnurhidayat271@gmail.com>
2024-05-16 00:18:44 +0800

    add menu staff in admin (masih progres)

---
+++ PA_PEMVIS2024/akunStaff.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class akunStaff
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtKonfirmasiPassword = New System.Windows.Forms.TextBox()
+        Me.btnDaftar = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(41, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "username"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(44, 57)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(203, 20)
+        Me.txtUsername.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(41, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "password"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(44, 105)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(203, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(41, 141)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(102, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "konfirmasi password"
+        '
+        'txtKonfirmasiPassword
+        '
+        Me.txtKonfirmasiPassword.Location = New System.Drawing.Point(44, 157)
+        Me.txtKonfirmasiPassword.Name = "txtKonfirmasiPassword"
+        Me.txtKonfirmasiPassword.Size = New System.Drawing.Size(203, 20)
+        Me.txtKonfirmasiPassword.TabIndex = 1
+        '
+        'btnDaftar
+        '
+        Me.btnDaftar.Location = New System.Drawing.Point(172, 192)
+        Me.btnDaftar.Name = "btnDaftar"
+        Me.btnDaftar.Size = New System.Drawing.Size(75, 23)
+        Me.btnDaftar.TabIndex = 2
+        Me.btnDaftar.Text = "Daftar"
+        Me.btnDaftar.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(44, 192)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'akunStaff
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(305, 250)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnDaftar)
+        Me.Controls.Add(Me.txtKonfirmasiPassword)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "akunStaff"
+        Me.Text = "akunStaffvb"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtKonfirmasiPassword As System.Windows.Forms.TextBox
+    Friend WithEvents btnDaftar As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+End Class

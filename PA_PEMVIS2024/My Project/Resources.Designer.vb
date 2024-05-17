db6bfb72698a76572f2865408b0fbce0ebf08f5c
HubZun <fadlanpdh@gmail.com>
2024-05-18 00:51:47 +0800

    update again broo

--- PA_PEMVIS2024/My Project/Resources.Designer.vb
+++ PA_PEMVIS2024/My Project/Resources.Designer.vb
@@ -123,6 +123,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Home_PA_Pemvis1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Home_PA_Pemvis1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property plus__1_() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("plus (1)", resourceCulture)
